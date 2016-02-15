43ac701f1a772f716a61d5d6f64e1c91e2572d8c
rafaelnavarroprieto <rafaelnavarroprieto@gmail.com>
2016-02-15 08:45:24 +0100

    Fixed html

--- portfolio/js/menu.js
+++ portfolio/js/menu.js
@@ -13,13 +13,4 @@ function toggleMenu (event) {
 // event
 menu.addEventListener('click', toggleMenu, false);
 
-//Solución con jQUery
-/*$(document).ready(function(){
-	$('.hamburger').click(function() {
-		$('.hamburger').toggleClass('is-active');
-		$('.menuresponsive').toggleClass('is-active');
-		return false;
-	});
-});*/
-
 });
